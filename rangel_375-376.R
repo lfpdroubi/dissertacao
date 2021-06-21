b4a53fffa68a96c7f33c5508f1e3d69c0ab06b2f
lfpdroubi <lfpdroubi@gmail.com>
2021-06-21 12:25:30 -0300

    to droplet

---
+++ rangel_375-376.R
@@ -0,0 +1,13 @@
+\begin{citacao}
+Vemos muitas vezes as municipalidades das grandes cidades se lançarem a obras
+dispensiosas no aterro de pântanos ou alagados próximos ao centro urbano. Como
+resultado, teremos, afinal, um pântano a menos e um terreno capaz de suportar
+construções, a mais. Num país tão grande como o nosso, pode parecer absurdo que
+se pretenda empregar recursos escassos para a "produção" de terrenos quando, com
+despesas menores, seria possível construir uma estrada que tornasse acessíveis 
+terrenos naturalmente bons ou quando, no próprio centro urbano, há ainda 
+terrenos inocupados. O projeto não estará plenamente justificado, entretanto,
+enquanto não consideremos que o pântano era, em si mesmo, uma desutilidade, um
+estorvo ou uma fonte de malária e que, portanto, a riqueza do páis aumenta pelo
+simples fato de o fazermos desaparecer.
+\end{citacao}
